362e97e4518f9291a00fae8f12f1f73036e502ed
Kien Vuong <kienvuong@Kiens-MacBook-Pro.local>
2018-04-19 10:39:36 +0700

    no message

--- Mines-Game/Unity3d-Client/Mines/Assets/Scripts/BackToHome.cs
+++ Mines-Game/Unity3d-Client/Mines/Assets/Scripts/BackToHome.cs
@@ -7,15 +7,12 @@ using UnityEngine.UI;
 
 public class BackToHome : MonoBehaviour {
 
-	[DllImport("__Internal")]
-	private static extern void GoToHome();
-
 	// Use this for initialization
 	void Start () {
         GetComponent<Button>().onClick.AddListener(OnButtonClick);	
 	}
 	
     public void OnButtonClick(){
-        GoToHome();
+        SceneManager.LoadScene("GRDLogin");
     }
 }
